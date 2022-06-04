e18396a5895132dce8d58b57fc361bca70fd8215
junseok <njs702@ajou.ac.kr>
2022-06-04 23:07:44 +0900

    0604 coding test practice

---
+++ 0604/15663.py
@@ -0,0 +1,32 @@
+import sys
+
+N,M = map(int,sys.stdin.readline().split())
+numbers = list(map(int,sys.stdin.readline().split()))
+_result = []
+numbers.sort()
+
+
+visited = [False for _ in range(N)]
+
+def dfs():
+    if len(_result) == M:
+        print(' '.join(map(str,_result)))
+        return
+    
+    pre = 0
+    for i in range(N):
+        
+        # 같은 숫자 한번 더 쓰는 거 방지
+        if visited[i] : continue
+        
+        # 배열 안에 같은 숫자가 있을 경우, 중복 출력 방지
+        if pre == numbers[i] : continue
+        
+        _result.append(numbers[i])
+        visited[i] = True
+        pre = numbers[i]
+        dfs()
+        _result.pop()
+        visited[i] = False
+
+dfs()
